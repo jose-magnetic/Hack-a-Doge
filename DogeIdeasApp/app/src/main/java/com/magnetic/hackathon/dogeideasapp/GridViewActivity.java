237e06de628ab6fa0423a8372ad3e4a56992b91e
Yen-Ting <yen-ting.liu@magnetic.com>
2016-04-21 15:10:40 -0700

    Can change from Cat to Dog and vice versa

--- DogeIdeasApp/app/src/main/java/com/magnetic/hackathon/dogeideasapp/GridViewActivity.java
+++ DogeIdeasApp/app/src/main/java/com/magnetic/hackathon/dogeideasapp/GridViewActivity.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -30,18 +34,73 @@ public class GridViewActivity extends Activity {
 
     // Oracle queries
     private static final String GRID_ITEM_QUERY = "select m_display_name, m_price, m_image_url, m_detail_url from ETLPREP.DOGE";
+    private static final String GRID_ITEM_QUERY_CAT = "select m_display_name, m_price, m_image_url, m_detail_url from ETLPREP.CATS";
+
+    private enum State {Dogs, Cat}
+
+    State state;
 
     private GridView mGridView;
     private ProgressBar mProgressBar;
 
+    private FloatingActionButton dogButton;
+    private FloatingActionButton catButton;
+    private FloatingActionButton profileButton;
+
 
     private GridViewAdapter mGridAdapter;
     private List<GridItem> mGridData = new ArrayList<>(1000);
+    private List<GridItem> mGridDataCat = new ArrayList<>(1000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gridview);
+
+        state = State.Dogs;
+
+        dogButton = (FloatingActionButton) findViewById(R.id.button2);
+        catButton = (FloatingActionButton) findViewById(R.id.button3);
+
+        dogButton.setVisibility(View.GONE);
+        catButton.setVisibility(View.GONE);
+
+        profileButton = (FloatingActionButton) findViewById(R.id.button);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation slideLeft = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.side_left);
+                Animation slideLeft2 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.side_left2);
+                dogButton.setAnimation(slideLeft);
+                dogButton.setVisibility(View.VISIBLE);
+                catButton.setAnimation(slideLeft2);
+                catButton.setVisibility(View.VISIBLE);
+            }
+        });
+
+        dogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGridAdapter.setGridData(mGridDataCat);
+                mGridAdapter.notifyDataSetChanged();
+                mGridView.invalidateViews();
+                state = State.Cat;
+                dogButton.setVisibility(View.GONE);
+                catButton.setVisibility(View.GONE);
+            }
+        });
+
+        catButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mGridAdapter.setGridData(mGridData);
+                mGridAdapter.notifyDataSetChanged();
+                mGridView.invalidateViews();
+                state = State.Dogs;
+                dogButton.setVisibility(View.GONE);
+                catButton.setVisibility(View.GONE);
+            }
+        });
 
         mGridView = (GridView) findViewById(R.id.gridView);
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
@@ -111,6 +170,17 @@ public class GridViewActivity extends Activity {
                     item.setSiteURL(rs.getString(4));
 
                     mGridData.add(item);
+                }
+
+                rs = st.executeQuery(GRID_ITEM_QUERY_CAT);
+                while (rs.next()) {
+                    GridItem item = new GridItem();
+                    item.setTitle(rs.getString(1));
+                    item.setPrice(rs.getDouble(2));
+                    item.setImageURL(rs.getString(3));
+                    item.setSiteURL(rs.getString(4));
+
+                    mGridDataCat.add(item);
                 }
 
                 isSuccessful = true;
