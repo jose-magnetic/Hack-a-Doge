a8a7a68a8780306e2a817148b334ec1031b5997a
Jose Martinez <jose.martinez@magnetic.com>
2016-04-20 17:57:30 -0700

    Latest version

---
+++ DogeIdeasApp/app/src/main/java/com/magnetic/hackathon/dogeideasapp/GridItem.java
@@ -0,0 +1,35 @@
+package com.magnetic.hackathon.dogeideasapp;
+
+public class GridItem {
+    private String imageURL;
+    private String siteURL;
+    private String title;
+
+    public GridItem() {
+        super();
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSiteURL() {
+        return siteURL;
+    }
+
+    public void setSiteURL(String siteURL) {
+        this.siteURL = siteURL;
+    }
+}
---
+++ DogeIdeasApp/app/src/main/java/com/magnetic/hackathon/dogeideasapp/GridViewActivity.java
@@ -0,0 +1,64 @@
+package com.magnetic.hackathon.dogeideasapp;
+
+import java.util.ArrayList;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class GridViewActivity extends ActionBarActivity {
+    private static final String TAG = GridViewActivity.class.getSimpleName();
+
+    private GridView mGridView;
+
+    private GridViewAdapter mGridAdapter;
+    private ArrayList<GridItem> mGridData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gridview);
+
+        mGridView = (GridView) findViewById(R.id.gridView);
+
+        //Initialize with empty data
+        mGridData = new ArrayList<>();
+        mGridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, mGridData);
+        mGridView.setAdapter(mGridAdapter);
+
+        //Grid view click event
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                //Get item at position
+                GridItem item = (GridItem) parent.getItemAtPosition(position);
+
+                Intent intent = new Intent(GridViewActivity.this, DetailsActivity.class);
+                ImageView imageView = (ImageView) v.findViewById(R.id.grid_item_image);
+
+                // Interesting data to pass across are the thumbnail size/location, the
+                // resourceId of the source bitmap, the picture description, and the
+                // orientation (to avoid returning back to an obsolete configuration if
+                // the device rotates again in the meantime)
+
+                int[] screenLocation = new int[2];
+                imageView.getLocationOnScreen(screenLocation);
+
+                //Pass the image title and url to DetailsActivity
+                intent.putExtra("left", screenLocation[0]).
+                        putExtra("top", screenLocation[1]).
+                        putExtra("width", imageView.getWidth()).
+                        putExtra("height", imageView.getHeight()).
+                        putExtra("title", item.getTitle()).
+                        putExtra("image", item.getImageURL());
+
+                //Start details activity
+                startActivity(intent);
+            }
+        });
+
+    }
+}
